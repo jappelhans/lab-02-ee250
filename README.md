1b8c4f8a5d00970bf3815d010b0f26787f2f216d
jappelhans <appelhan@usc.edu>
2023-01-24 23:49:29 -0800

    finished tcp_client.cpp

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 [Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) this repo and clone it to your machine to get started!
 
 ## Team Members
-- team member 1
-- team member 2
+- Jerry Appelhans
+- Riad Alasadi
 
 ## Lab Question Answers
 
 Answer for Question 1: 
+    With a 50% loss added to the UDP server, about half of the messages that the client try to send to the server are lost.  These messages are completely dropped, and they never arrive at the server.
+
+Answer for Question 2:
+    With a 50% loss added to the TCP server, the reliability of the communication link stays the same.  Every message that the client sends is eventually received by the server.  When a message is lost, it is retransmitted instead of just being dropped. 
+
+Answer for Question 3:
+    With the 50% loss, the speed of the TCP response is much slower.  This happens because when a message is dropped, TCP spends the extra time to retransmit the lost message before it attempts to send new messages.
+
 
 ...
--- tcp_client.cpp
+++ tcp_client.cpp
@@ -10,19 +10,25 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+//using namespace std;
+
 int main(int argc, char const *argv[]) 
 { 
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
 	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	std::string server_ip = argv[1];
+	std::string server_port = argv[2];
+	printf("server ip: %s\n", server_ip.c_str());
+	printf("server port: %s\n", server_port.c_str());
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
@@ -44,10 +50,31 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
+	//int connectRes = connect(client_fd, (sockaddr*) server_addr, sizeof(server_addr));
+	int connectRes = connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen);
+	if (connectRes == -1) {
+		printf("failed to connect\n");
+		return 1;
+	}
+
 	// TODO: Retreive user input
+	std::string userInput;
+	printf("CLIENT> ");
+	std::getline(std::cin, userInput);
+
 	// TODO: Send() the user input to the server
+	int sendRes = send(client_fd, userInput.c_str(), userInput.size()+1, 0);
+	if (sendRes == -1)
+		printf("Could not send to server");
+
 	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+	memset(socket_read_buffer, '\0', 1024);
+	// Because I filled the entire socket_read_buffer with NULL characters, the string is guaranteed to be null terminated.
+	int bytesReceived = recv(client_fd, socket_read_buffer, 1024, 0);
+	printf("SERVER> %s\n", std::string(socket_read_buffer, bytesReceived).c_str());
+
 	// TODO: Close() the socket
+	close(client_fd);
 
 	return 0; 
 } 
