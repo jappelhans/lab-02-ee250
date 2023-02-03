85ce2ec1a814b1efa35ae07c34bb3e05d8ee5d64
jappelhans <appelhan@usc.edu>
2023-02-02 21:47:18 -0800

    final commit

--- README.md
+++ README.md
@@ -3,18 +3,16 @@
 
 ## Team Members
 - Jerry Appelhans
-- n/a
+- n/a (worked individually)
 
 ## Lab Question Answers
 
 Answer for Question 1: 
-    With a 50% loss added to the UDP server, about half of the messages that the client try to send to the server are lost.  These messages are completely dropped, and they never arrive at the server.
+    With a 50% loss added to the UDP server, about half of the messages that the client tries to send to the server are lost.  When these messages are lost, they are completely dropped and therefore never arrive at the server.
 
 Answer for Question 2:
-    With a 50% loss added to the TCP server, the reliability of the communication link stays the same.  Every message that the client sends is eventually received by the server.  When a message is lost, it is retransmitted instead of just being dropped. 
+    With a 50% loss added to the TCP server, the reliability is affected differently than the UDP link.  With TCP, every message that the client sends is eventually received by the server.  When a message is lost, it is retransmitted instead of just being dropped.  In this way, the reliability is much better than UDP, but the drawback is a slower speed.
 
 Answer for Question 3:
-    With the 50% loss, the speed of the TCP response is much slower.  This happens because when a message is dropped, TCP spends the extra time to retransmit the lost message before it attempts to send new messages.
+    With the 50% loss, the speed of the TCP response is much slower.  This happens because when a message is dropped, TCP spends the extra time to retransmit the lost message before it attempts to send new messages.  The lost messages are able to be detected because TCP serializes each message unlike UDP.
 
-
-...
--- tcp_server.c
+++ tcp_server.c
@@ -18,17 +18,17 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
     /* 1. What is argc and *argv[]?
-     *    argc represents the number of arguments passed to the program from the environment in which it is run.  *argv[] is a pointer to the first element of an array of the arguments for the program.
+     *    argc represents the number of arguments passed to the program from the environment in which it is run.  *argv[] is a pointer to the first element of an array of the arguments for the program. In this program, argv[1] is the port number that we use to open the server.
      */
     int sockfd, newsockfd, portno;
     /* 2. What is a UNIX file descriptor and file descriptor table?
-     *    A UNIX file descriptor is a positive integer value that represents an open file being used in a process.  A file descriptor table is a collection of file descriptors and the pointers to the files that each descriptor represents.
+     *    A UNIX file descriptor is a positive integer value that represents an open file being used in a process.  A file descriptor table is a collection of file descriptors and the pointers to the corresponding files that each descriptor represents.
      */
     socklen_t clilen;
 
     struct sockaddr_in serv_addr, cli_addr;
     /* 3. What is a struct? What's the structure of sockaddr_in?
-     *    A struct is a user-defined data type that allows for grouping related variables.  sockaddr_in contains the variables for the server family, address, and port.
+     *    A struct is a user-defined data type that allows for grouping related variables for convenience.  sockaddr_in contains the variables for the server family, address, and port of a socket.
      */
     
     int n;
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     /* 4. What are the input parameters and return value of socket()
-     *    The input parameters of socket() are domain, type, and protocol. In this case, the domain parameter is AF_INET, which specifies that the socket will be able to communicate with IPv4 addresses. The type parameter specifies the type of socket to be created (SOCK_STREAM in this case).  The protocol parameter can be used to specify a specific protocol.  In this case, it is 0 which means the socket will use the default protocol for the given address family and socket type.
+     *    The input parameters of socket() are domain, type, and protocol. In this case, the domain parameter is AF_INET, which specifies that the socket will be able to communicate with IPv4 addresses. The type parameter specifies the type of socket to be created (SOCK_STREAM in this case).  The protocol parameter can be used to specify a specific communication protocol.  In this case, it is 0 which means the socket will use the default protocol for the given address family and socket type.
      *    The return value of socket() is a file descriptor that can be used later on to refer to the socket that the function creates. Because the function returns a file descriptor, we can check to see if socket() worked by making sure sockfd is positive.
      */
     
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
                     (struct sockaddr *) &cli_addr, 
                     &clilen);
 	/* 7. Research how the command fork() works. How can it be applied here to better handle multiple connections?
-         *    The fork() function creates a child process that runs in sync with the parent program. This could be applied here by making a child process for each incoming connection. This would allow the server to better handle multiple simultaneous incoming connections.
+         *    The fork() function creates a child process that runs in sync with the parent program. This could be applied here by making a child process for each new incoming connection. This would allow the server to better handle multiple simultaneous incoming connections instead of putting them in a queue.
          */
         
 	if (newsockfd < 0) 
@@ -93,5 +93,5 @@ int main(int argc, char *argv[])
 }
   
 /* This program makes several system calls such as 'bind', and 'listen.' What exactly is a system call?
- * A system call is a function in a program that requests a service from the operating system. This is necessary for networking because the program needs to make changes to things that the operating system has control of like opening sockets and communicating with other computers.
+ * A system call is a function in a program that requests an external service from the operating system. This is necessary for networking because the program needs to make changes to things that the operating system has control of like opening sockets and sending data to other computers over the network.
  */
